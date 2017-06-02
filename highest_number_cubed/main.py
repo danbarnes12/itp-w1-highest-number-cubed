a6d02bb523e0373daea16ff4e2af2b848873bdeb
Dan Barnes <danbarnes12@gmail.com>
2017-06-02 00:08:00 +0000

    First Commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,9 @@
 """This is the entry point of the program."""
 
-
 def highest_number_cubed(limit):
-    pass
+    number = -1
+    test = 0
+    while test < limit:
+        number += 1
+        test = number * number * number
+    return (number - 1)
